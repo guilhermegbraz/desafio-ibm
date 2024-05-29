f8ddd7447cd5bc43475c3eeba2da24c826d87585
Guilherme <guilherme.g.braz@outlook.com>
2024-05-28 21:05:05 -0300

    setup banco dev

---
+++ src/main/resources/db/migration/V01__create_table_cliente_e_table_conta_bancaria.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cliente(
+    id int not null auto_increment primary key,
+    idade int not null,
+    endereco_email varchar(100) not null unique,
+    numero_conta varchar(10) unique not null
+);
+
+CREATE TABLE conta_bancaria(
+    id int not null auto_increment primary key,
+    numero_conta varchar(10) unique not null ,
+    id_cliente int not null,
+    saldo DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    credito_disponivel DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
+);
