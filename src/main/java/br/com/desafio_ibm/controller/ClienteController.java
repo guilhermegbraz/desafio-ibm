ccc298ae0211a1d06b36913a65e71901f0dbe681
Guilherme <guilherme.g.braz@outlook.com>
2024-05-28 21:05:42 -0300

    setup controller

--- src/main/java/br/com/desafio_ibm/controller/ClienteController.java
+++ src/main/java/br/com/desafio_ibm/controller/ClienteController.java
@@ -3,9 +3,11 @@ package br.com.desafio_ibm.controller;
 import br.com.desafio_ibm.dto.ViewClienteDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/cliente")
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
 
     @GetMapping
